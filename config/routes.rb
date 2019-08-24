8c39940174f07f412dfe3fe1933ae02e523aaae6
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-24 12:57:27 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'appointments/show'
+
+  get 'appointments/index'
+
+  get 'appointments/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
